f319a064f01126740c46ddba3f48aee687ec90a5
TheFaded-Greg-L <gregory.lui23@ycdsbk12.ca>
2022-04-14 08:43:57 -0400

    Completed

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,33 @@
 import processing.core.PApplet;
-
+import processing.core.PImage;
+/**
+ * A program, "Sketch.java", that draws a person with objects circling around them
+ * @author G. Lui
+ */
 public class Sketch extends PApplet {
+  // global variables
+  float personX = 0;
+  float personY = 700;
+  float personWidth = 100; 
+  float personHeight = 100; 
+  int personSpeed = 10;
+
+  float circlingRotation = 1;
 	
-	
+  // images
+  PImage backgroundImage;
+
+	PImage personRight;
+  PImage personLeft;
+  PImage person;
+  PImage Block;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1000, 1000);
   }
 
   /** 
@@ -16,21 +35,70 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    frameRate(60);
+    background(0, 0, 0);
+
+    // load images
+    backgroundImage = loadImage("Background Image.png");
+
+    personRight = loadImage("Person Right.png");
+    personLeft = loadImage("Person Left.png");
+    person = personRight;
+    Block = loadImage("Block.png");
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	  image(backgroundImage, 0, 0, width, height);
+    personMoving();
+    circlingShapes();
   }
   
   // define other methods down here.
+
+  /**
+   * Draws the person moves
+   * @author G. Lui
+   */
+  public void personMoving(){
+
+    personX += personSpeed;
+    image(person, personX, personY, personWidth, personHeight);
+
+    // right border collision
+    if (personX + personWidth >= width){
+
+      personSpeed *= -1;
+      person = personLeft;
+    }
+
+    // left border collision
+    if (personX <= 0){
+
+      personSpeed *= -1;
+      person = personRight;
+    }  
+
+
+  }
+
+  /**
+   * Draws the circling objects around the person
+   * @author G. Lui
+   */
+  public void circlingShapes(){
+
+    pushMatrix();
+    translate(personX + (personWidth / 2) - 10, personY + (personHeight / 2) + 5);
+    rotate(radians(circlingRotation));
+
+    stroke(150, 11, 36);
+    fill(171, 17, 45);
+    ellipse(75, 75, 50, 50);
+    image(Block, -115, -115, 50, 50);
+    popMatrix();
+    circlingRotation += 5;
+  }
 }
